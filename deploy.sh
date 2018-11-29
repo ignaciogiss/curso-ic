5376e5f8f925f2f3dc89b0f0a69bdcd26e9dd718
IGNACIO GISS <igiss@ar0fvfxf19ahv2j.ml.com>
2018-11-29 11:35:35 -0300

    hace que test local funcine

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop curso
 docker rm curso
-docker run --link sql-server:sql-server --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT 
+docker run --link some-redis:some-redis --link sql-server:sql-server --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT 
